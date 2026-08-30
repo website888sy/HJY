a744140bd332658179ca32588bfb1711aa02d1b0
HJY <saeed.alqassar@gmail.com>
2026-08-30 04:25:45 +0300

    تحديث كاش الموقع v4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* HJY site service worker - cache-first for static assets & photos */
-const VERSION = "hjy-site-v3";
+const VERSION = "hjy-site-v4";
 const STATIC_CACHE = VERSION + "-static";
 const PHOTO_CACHE = VERSION + "-photo";
 
